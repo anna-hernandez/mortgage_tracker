8f4bf202f5a457a548e98309dd3d115a9330c80d
anna <annahernandezduran@gmail.com>
2018-11-13 09:43:20 +0000

    commented

--- mortgage_tracker.py
+++ mortgage_tracker.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-
+# define global variables
 borrowed = 273000.0
 interest_rate = 0.025
 mortgage_length = 25
@@ -18,13 +18,15 @@ repayment_divident = (1.0-(1/(1+interest_rate)**mortgage_length))*(1/interest_ra
 annual_repayment = borrowed * 1.0/repayment_divident
 monthly_repayment = annual_repayment/12.0
 
+# create tracking arrays
 interests = np.empty((0, mortgage_length))
 lefttopay = np.empty((0, mortgage_length))
 capitals = np.empty((0, mortgage_length))
 
 working_borrowed = borrowed
+
+# loop through mortgage years to calculate the payments per year
 for year in range(mortgage_length):
-    #print('Year',year+1)
     on_interests = working_borrowed*0.025
     interests = np.append(interests, on_interests)
     on_capital = annual_repayment - on_interests
@@ -37,15 +39,20 @@ for year in range(mortgage_length):
 
     working_borrowed -= on_capital
     lefttopay = np.append(lefttopay, working_borrowed)
-    print('Left to pay: {}'.format(working_borrowed))
+    print('Year {}: {} to pay, {} on interests, {} on capital. Left to pay: {}'.format(year+1, round(annual_repayment,2), round(on_interests,2), round(on_capital,2), round(working_borrowed,2)))
 
-mortgage = pd.DataFrame({'interest': interests, 'capital':capitals, 'pay':lefttopay})
+# --------------------- MAKE PLOT --------------------------
+
+# create figure
 plt.figure(1, figsize=(10,5))
 
+# add stacked bar plot
 p1 = plt.bar(np.arange(1,mortgage_length+1), lefttopay, alpha=0.8)
 p2 = plt.bar(np.arange(1,mortgage_length+1), capitals, bottom=lefttopay, alpha=0.8)
 p3 = plt.bar(np.arange(1,mortgage_length+1), interests, bottom=lefttopay+capitals, alpha=0.8)
 p4 = plt.plot(np.arange(1,mortgage_length+1), [annual_repayment]*mortgage_length,color='black', linestyle='--', linewidth=1)
+
+# add legend and labels, adjust y axis limits
 plt.legend((p1[0], p2[0], p3[0], p4[0]), ('Left to pay', 'Capital repayment', 'Interest repayment', 'Annual repayment'))
 plt.title('Mortgage repayment over time')
 plt.xlabel('Time (years)')
@@ -53,6 +60,8 @@ plt.ylabel('Amount (currency)')
 plt.yticks(np.arange(0, 300001, 50000))
 plt.xticks(np.arange(1,mortgage_length+1,1))
 plt.ylim(top=300000)
+
+# show
 plt.show()
 
 
